785410b4a1d25cf044cc16aa0cf55b01d215eaec
mehmet soner korucu22 <mehmetsonerkorucu@hotmail.com>
2023-03-17 00:01:21 +0300

    gnl and mlx added

--- Cub3d/lib/cub3d.h
+++ Cub3d/lib/cub3d.h
@@ -1,8 +1,6 @@
 #ifndef CUB3D_H
 #define CUB3D_H
 
-#include "libft/libft.h"
-
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
@@ -10,10 +8,13 @@
 # include <math.h>
 # include "mlx/mlx.h"
 # include "libft/libft.h"
+# include "gnl/get_next_line.h"
+
+# define EXTENTION_ERROR 1
 
 typedef	struct	s_cub3d
 {
-	//...
+	char	**map;
 }				t_cub3d;
 
 #endif
