f0edcdec40dc4013e0c8159e500cfff0cf374f25
mehmet soner korucu22 <mehmetsonerkorucu@hotmail.com>
2023-03-18 20:28:33 +0300

    🔧🔧 in check_directions

--- Cub3d/checkers/check_directions.c
+++ Cub3d/checkers/check_directions.c
@@ -1,9 +1,10 @@
 #include "../lib/cub3d.h"
 
-void	set_direction(t_cub3d *cub, char **str, int i)
+static void	set_direction(t_cub3d *cub, char **str, int i,int *check)
 {
 	if (!*str)
 	{
+		(*check)++;
 		*str = ft_strdup(&cub->map_file[i][2]);
 		*str = ft_strtrim(*str, " ");
 	}
@@ -13,20 +14,25 @@ void	set_direction(t_cub3d *cub, char **str, int i)
 int	check_directions(t_cub3d *cub)
 {
 	int	i;
+	int	check;
 
 	i = 0;
+	check = 0;
 	while(cub->map_file[i])
 	{
 		cub->map_file[i] = ft_strtrim(cub->map_file[i], " ");
 		if (!ft_strncmp(cub->map_file[i], "EA",2))
-			set_direction(cub, &cub->east, i);
+			set_direction(cub, &cub->east, i, &check);
 		if (!ft_strncmp(cub->map_file[i], "SO",2))
-			set_direction(cub, &cub->south, i);
+			set_direction(cub, &cub->south, i, &check);
 		if (!ft_strncmp(cub->map_file[i], "NO",2))
-			set_direction(cub, &cub->north, i);
+			set_direction(cub, &cub->north, i, &check);
 		if (!ft_strncmp(cub->map_file[i], "WE",2))
-			set_direction(cub, &cub->west, i);
+			set_direction(cub, &cub->west, i, &check);
 		i++;
 	}
+	if (i != 4)
+		return (DIRECTION_MISMATCH);
+	
 	return 0;
 }
